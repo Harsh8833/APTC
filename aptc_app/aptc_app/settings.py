5b5ce1dd4b502c7e18a95d424ec3d1db6159c175
Dewansh07 <dewansh.2021@vitbhopal.ac.in>
2022-02-23 20:18:37 +0530

    initial

--- aptc_app/aptc_app/settings.py
+++ aptc_app/aptc_app/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'Asia/Kolkata'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
