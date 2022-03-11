510e73ba8fc10d20ed066610b7792e364823ec78
Dewansh07 <dewansh.2021@vitbhopal.ac.in>
2022-03-11 20:42:44 +0530

    model

--- aptc_app/student/models.py
+++ aptc_app/student/models.py
@@ -1,23 +1,12 @@
 from django.db import models
 from datetime import datetime
 
-class_choice = (
-    ("1", "11"),
-    ("2", "12"),
-)
-board_choice = (
-    ("1", "CBSE"),
-    ("2", "BSEB"),
-)
-gender_choice = (
-    ("1", "Male"),
-    ("2", "Female"),
-)
+
 class students_detail(models.Model):
-    sclass=models.CharField(max_length=120, choices=class_choice, default='')
-    board=models.CharField(max_length=120, choices=board_choice, default='')
+    sclass=models.CharField(max_length=120)
+    board=models.CharField(max_length=120)
     name=models.CharField(max_length=50)
-    gender=models.CharField(max_length=120, choices=gender_choice, default='')
+    gender=models.CharField(max_length=120)
     dob=models.DateField(auto_now_add=True, auto_now=False, blank=True)
     fname=models.CharField(max_length=50)
     phone=models.IntegerField()
